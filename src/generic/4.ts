cffae65df9301614fac4a0c939be684ce4189715
YuriyBeregovoy <rik0681@gmail.com>
2023-10-15 22:40:07 +0300

    Update 4.ts

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,15 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+type PageProps = { title: string; }
+
+class Component<T>  {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<PageProps> {
   pageInfo () {
     console.log(this.props.title);
   }
